9b1cad94f3f32b50d0dd58d399f75935c0212c88
+ hi, it's molly (✿ ♡‿♡) <mollyannegustaf@gmail.com>
2026-03-09 23:04:26 -0600

    Create cyclone.py

---
+++ cyclone.py
@@ -0,0 +1,12 @@
+height = int(input("What is your height in cm? "))
+
+credits = int(input("How many credits do you have? "))
+
+if height >= 137 and credits >= 10:
+  print("Enjoy the ride!")
+elif height < 137 and credits >= 10:
+  print("You are not tall enoguh to ride.")
+elif height >= 137 and credits < 10:
+  print("You don't have enough credits.")
+else:
+  print("You are not tall enough and do not have enough credits.")
