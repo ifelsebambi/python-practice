e07e41046b2e5aad28ed0feeadc5ead640bb6c4d
+ hi, it's molly (✿ ♡‿♡) <mollyannegustaf@gmail.com>
2026-03-13 17:46:05 -0600

    Add countdown for 99 Bottles of Beer song

---
+++ 99bottles.py
@@ -0,0 +1,12 @@
+#countdown iteration 
+
+bottles = 100
+
+
+for bottles in range(99, 0, -1):
+  print(f'{bottles} bottles of beer on the wall')
+  print(f'{bottles} bottles of beer')
+  print('Take one down, pass it around')
+  
+
+print('No more bottles of beer on the wall')
